5264adb4815e30775d5335a09d92ec1edf66c776
yakol <ya.kolysh@gmail.com>
2020-10-12 15:34:28 +0300

    Initial commit

---
+++ src/ru/mirea/Yana/laba5_13/Main.java
@@ -0,0 +1,10 @@
+package ru.mirea.Yana.laba5_13;
+
+public class Main {
+
+    public static void main(String[] args) {
+        OddPos op = new OddPos();
+        op.FillArray();
+        op.ShowOddPosElements();
+    }
+}
